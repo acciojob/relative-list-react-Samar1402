a8f3a1611e9a5e82bb0ff98eb7da4fb1ae0bbd6e
Samarjeet <samarjeetpandey1402@gmail.com>
2024-07-26 20:44:01 +0530

    relatives assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
+const relatives = ["relatives1", "relatives2", "relatives3", "relatives4"];
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {relatives.map((relatives, i) => (
+          <li key={`relativesListItem&{i+1}`}>{relatives} </li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
